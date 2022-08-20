04bc938fa7fb949015436a3e98d08051c46cc91e
zjovic <38418292+zjovic@users.noreply.github.com>
2022-08-20 10:31:57 +0200

    Add shuffle button

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-window.addEventListener('load', () => {
-    let number = randomNumber();
-    let symbol = randomSymbol();
-
-    generateCardTemplate({ number, symbol });
-});
-
 const $card = document.querySelector('.card');
 const $symbolTop = document.createElement('div');
 const $symbolBottom = document.createElement('div');
 const $number = document.createElement('div');
+const $shuffle = document.querySelector('.shuffle');
 const spade = `&#9824;`;
 const club = `&#9827;`;
 const heart = `&#9829`;
 const diamonds = `&#9830;`;
 const symbols = [spade, club, heart, diamonds];
+
+window.addEventListener('load', () => {
+    generateCard();
+});
+
+$shuffle.addEventListener('click', () => {
+    generateCard();
+})
 
 function randomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
@@ -50,4 +52,11 @@ function generateCardTemplate({ number, symbol }) {
         $symbolTop.classList.add('red');
         $symbolBottom.classList.add('red');
     }
+}
+
+function generateCard() {
+    let number = randomNumber();
+    let symbol = randomSymbol();
+
+    generateCardTemplate({ number, symbol });
 }
