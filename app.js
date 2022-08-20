04dc447fa8e18f09eaab66b46ec26f43e64d2744
zjovic <38418292+zjovic@users.noreply.github.com>
2022-08-20 10:47:16 +0200

    Add resize card options

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const $symbolTop = document.createElement('div');
 const $symbolBottom = document.createElement('div');
 const $number = document.createElement('div');
 const $shuffle = document.querySelector('.shuffle');
+const $widthInput = document.querySelector('#width');
+const $heightInput = document.querySelector('#height');
 const spade = `&#9824;`;
 const club = `&#9827;`;
 const heart = `&#9829`;
@@ -15,7 +17,15 @@ window.addEventListener('load', () => {
 
 $shuffle.addEventListener('click', () => {
     generateCard();
-})
+});
+
+$widthInput.addEventListener('input', (e) => {
+    $card.style.minWidth = `${e.target.value}vw`;
+});
+
+$heightInput.addEventListener('input', (e) => {
+    $card.style.minHeight = `${e.target.value}vh`;
+});
 
 function randomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
